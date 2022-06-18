a957fa9353f85f54628294d3ed457196274ae249
DetrinoxYTB <107640076+DetrinoxYTB@users.noreply.github.com>
2022-06-18 13:07:24 -0300

    Update index.py

--- index.py
+++ index.py
@@ -7,17 +7,22 @@ number_generate = randint(1,1000)
 #Défini notre nombre de tentative
 tentative = 7
 
+#tant que tentative est supérieure à 0
 while tentative > 0:
+    #Vous demande de choisir un nombre
     choix = int(input("Veuillez choisir un nombre : "))
     
+    #Vérification
     if choix == number_generate:
         print("\nC'est Gagné !!\n")
         break
-        
+    
+    #Vérification
     elif choix > number_generate:
         print("\nC'est Moins !\n")
         tentative -= 1
-        
+    
+    #Vérification
     elif choix < number_generate:
         print("\nC'est Plus !\n")
         tentative -= 1
